4991f0e497c93b1cd5f4c9e6de12bb70d46a5a30
JunaeT420 <dkcks786@gmail.com>
2017-11-05 22:56:14 +0600

    Create a java class for development

---
+++ app/src/main/java/com/example/junae/googlemaptest/Asytask.java
@@ -0,0 +1,26 @@
+package com.example.junae.googlemaptest;
+
+/**
+ * Created by junae on 11/5/2017.
+ */
+
+public class Asytask {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
